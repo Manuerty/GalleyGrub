45b490ce27c11b0a0542bc5491203a4f8b913443
Manuerty <manuelcendonr@gmail.com>
2024-04-15 09:48:41 +0200

    feat (Extra)add Extra abstract class

---
+++ src/main/java/edu/badpals/galleyGrub/extras/Extra.java
@@ -0,0 +1,16 @@
+package edu.badpals.galleyGrub.extras;
+
+import java.util.Optional;
+import edu.badpals.galleyGrub.order.Command;
+
+public abstract class Extra{
+    static final String CHEESE = "Cheese";
+    static final String SAUCE = "Sauce";
+    static final String LARGE = "Large";
+    String extraProduct;
+    Optional<Extra> nextExtra;
+    public void setNextExtra(Extra extra) {
+        nextExtra = Optional.of(extra);
+    }
+    public void sumExtras(Command command) {}
+}
