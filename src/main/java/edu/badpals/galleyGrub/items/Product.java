b475e860d0e1347735ee2a73d8902cfad59ce90d
Manuerty <manuelcendonr@gmail.com>
2024-04-15 09:48:02 +0200

    feat (Product)add Product Interface

---
+++ src/main/java/edu/badpals/galleyGrub/items/Product.java
@@ -0,0 +1,12 @@
+package edu.badpals.galleyGrub.items;
+
+public interface Product {
+    public String name();
+
+    public Double price();
+
+    public String extra();
+
+    public Boolean isRegular();
+
+}
